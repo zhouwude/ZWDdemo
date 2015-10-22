1153fed897a17e342814079b4ab23ff827624360
zhouwude <572240202@qq.com>
2015-10-22 18:53:44 +0800

    zhouwude

--- ZWDdemo/AppDelegate.m
+++ ZWDdemo/AppDelegate.m
@@ -61,7 +61,6 @@
  */
 /**hehe */
 -(void)hehe{
-    UIView *view = [[UIView alloc] init];
 }
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
