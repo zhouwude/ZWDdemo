d7217d433b15cea38086317a59ef7d95def755f9
zhouwude <572240202@qq.com>
2015-11-25 11:11:17 +0800

    222

---
+++ Base/MLBaseView.h
@@ -0,0 +1,26 @@
+//  github: https://github.com/MakeZL/UIView-Category
+//  author: @email <120886865@qq.com>
+//
+//  UIView+MBIBnspectable.m
+//  MakeZL
+//
+//  Created by 张磊 on 15/7/3.
+//  Copyright (c) 2015年 www.weibo.com/makezl All rights reserved.
+//
+//  Ps: 感谢一位朋友(@ytx0574)告诉我，Xcode如果想实时监听MBIBnspectable的改变的话，需要继承一个空的基类，当然是无公害的.  如果需要的话请继承于MLBaseViewHeader.h
+//  参考@ytx0574提供的Demo:https://github.com/ytx0574/MBIBnspectable
+
+
+#import <UIKit/UIKit.h>
+
+@interface MLButton : UIButton
+@end
+
+@interface MLLabel : UILabel
+@end
+
+@interface MLView : UIView
+@end
+
+@interface MLImageView : UIImageView
+@end
---
+++ Base/MLBaseView.m
@@ -0,0 +1,29 @@
+//  github: https://github.com/MakeZL/UIView-Category
+//  author: @email <120886865@qq.com>
+//
+//  UIView+MBIBnspectable.m
+//  MakeZL
+//
+//  Created by 张磊 on 15/7/3.
+//  Copyright (c) 2015年 www.weibo.com/makezl All rights reserved.
+//
+//  Ps: 感谢一位朋友(@ytx0574)告诉我，Xcode如果想实时监听MBIBnspectable的改变的话，需要继承一个空的基类，当然是无公害的.  如果需要的话请继承于ML(Button/Label/View)
+//  参考@ytx0574提供的Demo:https://github.com/ytx0574/MBIBnspectable
+
+#import "MLBaseView.h"
+
+@implementation MLButton
+
+@end
+
+@implementation MLLabel
+
+@end
+
+@implementation MLView
+
+@end
+
+@implementation MLImageView
+
+@end
